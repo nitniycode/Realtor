d35f1ec0542863e02c7aef238dca3c119a7dfd7a
nitin yadav <ny941078@gmail.com>
2023-01-17 16:29:10 +0530

    Created Pages and Installed React-Router and Designed Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './PAGES/Home';
+import Offers from './PAGES/Offers';
+import Signup from './PAGES/Signup';
+import Signin from './PAGES/Signin';
+import ForgetPassword from './PAGES/ForgetPassword';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1 className="sm:bg-sky-500 md:bg-red-500 lg:bg-orange-700 max-lg:bg-violet-500 text-4xl">Hello</h1>
-  );
-}
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/forget-password" element={<ForgetPassword />} />
+        </Routes>
+      </Router>
+    </>
 
-export default App;
+  )
+}
+export default App
